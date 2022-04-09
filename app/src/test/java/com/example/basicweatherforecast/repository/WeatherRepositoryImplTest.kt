eaa99942030caabd5c2358d226382bcac5d992ab
Peeranont Jamornmarn <jamornmarn@gmail.com>
2022-04-10 05:02:28 +0700

    Refactor WeatherRepositoryImplTest

--- app/src/test/java/com/example/basicweatherforecast/repository/WeatherRepositoryImplTest.kt
+++ app/src/test/java/com/example/basicweatherforecast/repository/WeatherRepositoryImplTest.kt
@@ -6,12 +6,12 @@ import com.example.basicweatherforecast.data.model.GeolocationInfo
 import com.example.basicweatherforecast.data.remote.model.GeolocationModel
 import com.example.basicweatherforecast.data.remote.model.GeolocationResponse
 import com.example.basicweatherforecast.data.remote.service.OpenWeatherMapService
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.mockk
-import io.mockk.slot
+import io.mockk.*
+import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.runBlocking
+import org.junit.Before
 import org.junit.Test
+import org.koin.core.context.startKoin
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertNull
@@ -19,13 +19,23 @@ import kotlin.test.assertTrue
 
 class WeatherRepositoryImplTest {
 
+    lateinit var mWeatherRepository: WeatherRepository
+
+    @RelaxedMockK
+    lateinit var mOpenWeatherMapService: OpenWeatherMapService
+
+    @Before
+    fun setup(){
+        MockKAnnotations.init(this)
+        mWeatherRepository = WeatherRepositoryImpl(mOpenWeatherMapService)
+    }
+
+
     @Test
     fun `Get geolocation - Should success`() {
-        val service = mockk<OpenWeatherMapService>()
-        val repository = WeatherRepositoryImpl(service)
         val inputCity = "Bangkok"
         val slot = slot<String>()
-        coEvery { service.getGeolocation(capture(slot)) }.coAnswers {
+        coEvery { mOpenWeatherMapService.getGeolocation(capture(slot)) }.coAnswers {
             GeolocationResponse(
                 listOf(
                     GeolocationModel(inputCity, 13.7544238, 100.4930399, "TH", inputCity)
@@ -35,10 +45,10 @@ class WeatherRepositoryImplTest {
 
         var result: Result<Geolocation>
         runBlocking {
-            result = repository.getGeolocation(inputCity)
+            result = mWeatherRepository.getGeolocation(inputCity)
         }
 
-        coVerify(exactly = 1) { service.getGeolocation(any()) }
+        coVerify(exactly = 1) { mOpenWeatherMapService.getGeolocation(any()) }
         assertEquals(inputCity, slot.captured)
         assertTrue(result.isSuccess)
         assertEquals(
@@ -52,20 +62,18 @@ class WeatherRepositoryImplTest {
 
     @Test
     fun `Get geolocation - Should success with empty array because of city not found`() {
-        val service = mockk<OpenWeatherMapService>()
-        val repository = WeatherRepositoryImpl(service)
         val inputCity = "z"
         val slot = slot<String>()
-        coEvery { service.getGeolocation(capture(slot)) }.coAnswers {
+        coEvery { mOpenWeatherMapService.getGeolocation(capture(slot)) }.coAnswers {
             GeolocationResponse(listOf())
         }
 
         var result: Result<Geolocation>
         runBlocking {
-            result = repository.getGeolocation(inputCity)
+            result = mWeatherRepository.getGeolocation(inputCity)
         }
 
-        coVerify(exactly = 1) { service.getGeolocation(any()) }
+        coVerify(exactly = 1) { mOpenWeatherMapService.getGeolocation(any()) }
         assertEquals(inputCity, slot.captured)
         assertTrue(result.isSuccess)
         assertEquals(Geolocation(listOf()), result.data)
@@ -73,21 +81,19 @@ class WeatherRepositoryImplTest {
 
     @Test
     fun `Get geolocation - Should error with Exception`() {
-        val service = mockk<OpenWeatherMapService>()
-        val repository = WeatherRepositoryImpl(service)
         val inputCity = "test"
         val errorMessage = "Data not found"
         val slot = slot<String>()
-        coEvery { service.getGeolocation(capture(slot)) }.throws(
+        coEvery { mOpenWeatherMapService.getGeolocation(capture(slot)) }.throws(
             NullPointerException(errorMessage)
         )
 
         var result: Result<Geolocation>
         runBlocking {
-            result = repository.getGeolocation(inputCity)
+            result = mWeatherRepository.getGeolocation(inputCity)
         }
 
-        coVerify(exactly = 1) { service.getGeolocation(any()) }
+        coVerify(exactly = 1) { mOpenWeatherMapService.getGeolocation(any()) }
         assertEquals(inputCity, slot.captured)
         assertFalse(result.isSuccess)
         assertNull(result.data)
