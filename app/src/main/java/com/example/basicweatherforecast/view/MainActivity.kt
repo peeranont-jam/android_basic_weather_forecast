c233dd507304ae0c0400b9e090de54e410d77338
Peeranont Jamornmarn <jamornmarn@gmail.com>
2022-04-11 01:23:59 +0700

    Setup Android Navigation Component - Create CurrentWeatherFragment

--- app/src/main/java/com/example/basicweatherforecast/view/MainActivity.kt
+++ app/src/main/java/com/example/basicweatherforecast/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.basicweatherforecast
+package com.example.basicweatherforecast.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.basicweatherforecast.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
