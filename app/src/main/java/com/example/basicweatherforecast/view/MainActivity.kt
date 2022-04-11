58a2093a6fd6e0abe96d595a308d730a32efcdda
Peeranont Jamornmarn <jamornmarn@gmail.com>
2022-04-12 05:55:31 +0700

    Implement NavigationUI - For Top app bar      - Auto-generated back button      - Auto-generated App Bar's title based on Navigation's label

--- app/src/main/java/com/example/basicweatherforecast/view/MainActivity.kt
+++ app/src/main/java/com/example/basicweatherforecast/view/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.basicweatherforecast.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.basicweatherforecast.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
     }
 }
