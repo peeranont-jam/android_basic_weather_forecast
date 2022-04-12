d73e731e385f3a5c89be2bd600633799e9558af2
Peeranont Jamornmarn <jamornmarn@gmail.com>
2022-04-12 14:49:43 +0700

    Hide soft keyboard on search button click

--- app/src/main/java/com/example/basicweatherforecast/view/currentweather/CurrentWeatherFragment.kt
+++ app/src/main/java/com/example/basicweatherforecast/view/currentweather/CurrentWeatherFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -52,6 +53,7 @@ class CurrentWeatherFragment : Fragment() {
 
     private fun setupViews() {
         binding.btnSearch.setOnClickListener {
+            binding.etCityName.onEditorAction(EditorInfo.IME_ACTION_DONE)
             currentWeatherViewModel.getWeatherInfo(
                 binding.etCityName.text.toString(),
                 getSelectedTempUnit()
